a7dd630c46a85f13d34885cdb35a4e5d15205522
EmilieM <emilie.maillard@diiage.org>
2024-03-06 16:18:17 +0100

    Setup environment variables and API services

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:4200/api',
+  guardeepId: '376FAA9F-CB38-4C98-B954-84A24948405A',
+  guardeepReturnUri: 'http://localhost:4200/callback',
+};
---
+++ src/models/event.ts
@@ -0,0 +1,7 @@
+export interface User {
+  Id: string,
+  Title: string,
+  Description: string,
+  Date: string,
+  Location: string,
+}
